7a10bd5420f1308962683845fa9b3a3ea5e4243a
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-08-22 13:59:05 +0200

    Create connect.js

---
+++ config/connect.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST:'localhost',
+    USER:'postgres',
+    PASSWORD:'DizzyAttic',
+    DB:'postgres',
+    PORT:5432,
+    dialect:'postgres',
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
+}
