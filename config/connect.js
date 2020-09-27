d8927adb4012cb8ebae71de867ad7151bc137fa1
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-09-27 13:20:44 +0200

    added credentials

--- config/connect.js
+++ config/connect.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST:'localhost',
-    USER:'postgres',
-    PASSWORD:'DizzyAttic',
-    DB:'postgres',
+    HOST:'ec2-54-246-87-132.eu-west-1.compute.amazonaws.com',
+    USER:'vovrfxshwowfys',
+    PASSWORD:'601bf84f965a3edb497720ca8ce8c06fbf76d132b9d8b8f3aab42143ac6179e1',
+    DB:'df9o1pmlki4b90',
     PORT:5432,
     dialect:'postgres',
     pool:{
