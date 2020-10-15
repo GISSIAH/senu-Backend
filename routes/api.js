f36bf9ff4a20ef9d4dc041b0b3de7591f629e893
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-10-15 01:54:44 -0700

    rename

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,28 @@
 module.exports = app=>{
-    const tutorials = require('../controller/controller.js');
+    const ho = require('../controller/controller.js');
     var router =  require('express').Router();
     router.get('/',(req,res)=>{
         res.send('Welcome To BRYANS PROJECT');
     });
     //creates an entry
-    router.post('/',tutorials.create);
+    router.post('/',ho.create);
     //retrieves all entries
-    router.get('/all',tutorials.findAll);
+    router.get('/all',ho.findAll);
     ////retrieves all entries for a particular hospital
-    router.get('/all/:id',tutorials.findOneMany);
+    router.get('/all/:id',ho.findOneMany);
     //gets all recent attributes for every hospital 
-    router.get('/recent/',tutorials.findAllRecent);
+    router.get('/recent/',ho.findAllRecent);
      //gets all hour specific attributes for every hospital 
-    router.get('/specific/',tutorials.findAllSpecific);
+    router.get('/specific/',ho.findAllSpecific);
     //gets locations for all hospitals
-    router.get('/local',tutorials.getLocations);
+    router.get('/local',ho.getLocations);
     //gets recent attributes for every hospital with its location
-    router.get('/local/recent',tutorials.getRecent);
+    router.get('/local/recent',ho.getRecent);
     //gets the specific date-time attributes their locations
-    router.get('/local/specific',tutorials.getSpecific);
+    router.get('/local/specific',ho.getSpecific);
     //gets daily localation with attributes in groups
-    router.get('/local/group',tutorials.getGroup);
-    // router.delete('/:id',tutorials.delete);
-    // router.delete('/',tutorials.deleteAll);
+    router.get('/local/group',ho.getGroup);
+    // router.delete('/:id',ho.delete);
+    // router.delete('/',ho.deleteAll);
     app.use('/',router);
 }
